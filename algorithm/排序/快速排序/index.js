963cb6a34412a8a4501a22c98fde729db67917de
jie <levinjie2018@163.com>
2020-11-15 13:34:26 +0800

    feat: 增加快速排序的解法

--- algorithm/排序/快速排序/index.js
+++ algorithm/排序/快速排序/index.js
@@ -16,3 +16,30 @@ function quickSort(arr) {
   }
   return quickSort(left).concat(arr[target], quickSort(right));
 }
+
+//解法2
+function quickSort2(arr, start = 0, end) {
+  //recurse teminator
+  if(end - start < 1) {
+    return;
+  }
+  //each step same resolve the array
+  let end = r;
+  let start = l;
+  let target = arr[start];
+  while(l < r) {
+    while(l < r && arr[r] > target) {
+      r--;
+    }
+    arr[l] = arr[r];
+    while(l < r && arr[l] < target) {
+      l++;
+    }
+    arr[r] = arr[l];
+  }
+  arr[l] = target;
+  //dive into the recurse
+  quickSort2(arr, 0, l - 1);
+  quickSort2(arr, l + 1, end);
+  return arr;
+}
